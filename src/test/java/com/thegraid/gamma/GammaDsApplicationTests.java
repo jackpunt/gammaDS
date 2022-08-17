fe4fa2d3077f3cca360f2d34ce4dd3316c32580e
jackpunt <jpeckj@gmail.com>
2022-08-17 11:02:59 -0700

    initial, with lots of starters & HelloController

---
+++ src/test/java/com/thegraid/gamma/GammaDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegraid.gamma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GammaDsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
